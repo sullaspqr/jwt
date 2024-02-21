4e83b547300234afa3648522679af6101897ac1e
sullaspqr <sulla.spqr78@gmail.com>
2024-02-21 17:46:36 +0100

    first commit3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ function App() {
   const [token, setToken] = useState('');
   const [data, setData] = useState('');
 
+  const handleLogin = async() => {
+    try { 
+      const response = await axios.post('https://jwt.sulla.hu/login', { username, password});
+      setToken(response.data.token);
+    } catch (error) {
+      console.error("Hitelesítés sikertelen: ", error);
+    }
+  };
+  const fetchData = async() => {
+    try { 
+      const response = await axios.post('https://jwt.sulla.hu/termekek', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+      setData(response.data);
+    } catch (error) {
+      console.error("Adatok lekérése sikertelen: ", error);
+    }
+  };
 return (
   <div>
     <h1>Bejelentkezés</h1>
